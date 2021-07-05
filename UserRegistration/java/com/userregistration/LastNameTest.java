bbe0e03a7555a432e898f91f31050cf862eff113
Tushar Akhade <tusharakhade10@gmail.com>
2021-07-05 19:01:08 +0530

    Refactor to throw custom exceptions in case of invalid user details

---
+++ UserRegistration/java/com/userregistration/LastNameTest.java
@@ -0,0 +1,43 @@
+package com.userregistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Parameterized testing for last name
+ */
+@RunWith(Parameterized.class)
+public class LastNameTest {
+    private String lastNameTest;
+    private boolean expectedResult;
+
+    public LastNameTest(String lastName, boolean expectedResult) {
+        this.lastNameTest = lastName;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][] {
+                {"Akhade", true},
+                {"More", true},
+                {"akhade", false},
+                {"aKhade", false}
+        });
+    }
+
+    @Test
+    public void givenLastNameAsVar_ShouldReturnTrueOrFalse() {
+        try {
+            UserRegistration userRegistration = new UserRegistration(lastNameTest);
+            userRegistration.validateLastName(lastNameTest);
+        } catch (InvalidUserInputException e) {
+            Assert.assertEquals(InvalidUserInputException.ExceptionType.INVALID_LAST_NAME, e.type);
+        }
+    }
+}
